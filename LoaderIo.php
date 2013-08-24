d001982e8d363df5c92eeb569f645ed8233e5cf6
Nathan <web@nathanjenkins.com.au>
2013-08-25 07:17:20 +1000

    aditional functions

--- LoaderIo.php
+++ LoaderIo.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
  * @file
  * Simple PHP library for interacting with the loader.io api 
-*/
-  require ('vendor/nategood/Httpful/Bootstrap.php');
-
+ */
+require ('vendor/nategood/Httpful/Bootstrap.php');
 
 class LoaderIo {
-    
-    const API_KEY = '';//Your API Key here
-    const API_URL = 'https://api.loader.io/v2';
 
+    const API_KEY = ''; //Your API Key here
+    const API_URL = 'https://api.loader.io/v2';
 
     /**
      * Get all apps
@@ -37,6 +36,35 @@ class LoaderIo {
                 ->send();
         return $app->body;
     }
+    
+     /**
+     * Create a new app
+     * @param string $domain - the domain for the new app
+     */
+    public function createApp($domain) {
+        $newApp = Httpful\Request::post(self::API_URL . '/apps/')
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY,
+                ))
+                ->body(array('app' => $domain))
+                ->sendsType(Httpful\Mime::JSON)
+                ->send();
+        return $newApp->body;
+    }
+
+    /**
+     * Delete an existing loader.io app
+     * @param type $appId - The id of the app to be deleted
+     */
+    public function deleteApp($appId) {
+        $deleteApp = Httpful\Request::delete(self::API_URL . '/apps/' . $appId)
+                        ->addHeaders(array(
+                            'loaderio-auth' => self::API_KEY
+                        ))->send();
+        return $deleteApp->body;
+    }
+   
+    
 
     /**
      * Get all existing tests
@@ -51,10 +79,10 @@ class LoaderIo {
     }
 
     /**
-     * Get the details of an individual test
+     * Get the status of an individual test
      * @param string $testId The id of the test to be retrieved
      */
-    public function getTest($testId) {
+    public function getTestStatus($testId) {
         $test = Httpful\Request::get(self::API_URL . '/tests/' . $testId)
                 ->addHeaders(array(
                     'loaderio-auth' => self::API_KEY
@@ -62,18 +90,52 @@ class LoaderIo {
                 ->send();
         return $test->body;
     }
+
+    
+/**
+ * TODO - Send required values to get this working
+    public function createTest($initial = 0, $duration = 15, $total = 60, 
+            $urls = array('url' => 'domainhere.com')) {
+        $newTest = Httpful\Request::post(self::API_URL . '/tests/')
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY,
+                ))
+                ->body(array(
+                    'intial' => 0,
+                    'duration' => $duration,
+                    'total' => $total,
+                    'urls' => $urls
+                ))
+                ->sendsType(Httpful\Mime::JSON)
+                ->send();
+        return $newTest->body;
+    }
+*/
     /**
-     * Delete an existing loader.io app
-     * @param type $appId - The id of the app to be deleted
+     * Run a test
+     * @param string $testId - the id for the test to be executed
      */
-    public function deleteApp($appId){
-        $deleteApp = Httpful\Request::delete(self::API_URL . '/apps/'. $appId)
+    public function runTest($testId) {
+        $test = Httpful\Request::put(self::API_URL . '/tests/' . $testId . '/run')
                 ->addHeaders(array(
                     'loaderio-auth' => self::API_KEY
-                ))->send();
-        return $deleteApp->body;
+                ))
+                ->send();
+        return $test->body;
     }
-    
+
+    /**
+     * Stop a test
+     * @param string $testId - The id of the test to be stopped
+     */
+    public function stopTest($testId) {
+        $test = Httpful\Request::put(self::API_URL . '/tests/' . $testId . '/stop')
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY
+                ))
+                ->send();
+        return $test->body;
+    }
 
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ loaderio-php-library
 ====================
 
 PHP library for the [loader.io API](http://docs.loader.io/api/intro.html)
+
