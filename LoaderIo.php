c04c7f6a99ad3446b1374aae8de040f472484165
Nathan <web@nathanjenkins.com.au>
2013-08-24 19:55:53 +1000

    Initial commit

---
+++ LoaderIo.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @file
+ * Simple PHP library for interacting with the loader.io api 
+*/
+  require ('vendor/nategood/Httpful/Bootstrap.php');
+
+
+class LoaderIo {
+    
+    const API_KEY = '';//Your API Key here
+    const API_URL = 'https://api.loader.io/v2';
+
+
+    /**
+     * Get all apps
+     */
+    public function getAllApps() {
+
+        $allApps = Httpful\Request::get(self::API_URL . '/apps')
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY
+                ))
+                ->send();
+        return $allApps->body;
+    }
+
+    /**
+     * Get the details of an individual app
+     * @param string $app_id The unique app id
+     */
+    public function getApp($appId) {
+        $app = Httpful\Request::get(self::API_URL . '/apps/' . $appId)
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY
+                ))
+                ->send();
+        return $app->body;
+    }
+
+    /**
+     * Get all existing tests
+     */
+    public function getAllTests() {
+        $allTests = Httpful\Request::get(self::API_URL . '/tests')
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY
+                ))
+                ->send();
+        return $allTests->body;
+    }
+
+    /**
+     * Get the details of an individual test
+     * @param string $test_id The id of the test to be retrieved
+     */
+    public function getTest($testId) {
+        $test = Httpful\Request::get(self::API_URL . '/tests/' . $testId)
+                ->addHeaders(array(
+                    'loaderio-auth' => self::API_KEY
+                ))
+                ->send();
+        return $test->body;
+    }
+    
+
+}
+
+?>
